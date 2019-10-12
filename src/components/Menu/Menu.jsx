16e6056d7d30b9743abd9a6696bed4aa52f1ed6a
Bendik B. Iversen <bendikiv@gmail.com>
2019-10-12 19:48:38 +0200

    Versjon uten implementasjon av oppgave 3: meny

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -7,66 +7,32 @@ export const Menu = () => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <motion.nav className="Menu" initial={false} animate={isOpen ? "open" : "closed"}>
+        <nav className="Menu">
             <MenuArea />
             <MenuButton onClick={setIsOpen} clicked={isOpen}/>
-        </motion.nav>
+        </nav>
     )
 }
 
 const MenuArea = () => {
-    const variants_menuArea = {
-        open: {
-            opacity: 1,
-            x: 0,
-        },
-        closed: {
-            opacity: 0,
-            x: -200,
-            transition: {
-                delay: 0.5
-            }
-        }
-    };
-
     return (
-        <motion.div className="MenuArea" variants={variants_menuArea}>
+        <div className="MenuArea">
             <MenuNavigation/>
-        </motion.div>
+        </div>
     )
 }
 
 const MenuNavigation = () => {
-    const variants_navList = {
-        open: {
-          transition: { staggerChildren: 0.07, delayChildren: 0.2 }
-        },
-        closed: {
-          transition: { staggerChildren: 0.05, staggerDirection: -1 }
-        }
-    };
-
-    const variants_navItems = {
-        open: {
-            opacity: 1,
-            y: 0
-        }, 
-        closed: {
-            opacity: 0,
-            y: -50
-        }
-    }
-
     return (
-        <motion.ul className="MenuNavigation" variants={variants_navList}>
+        <ul className="MenuNavigation">
             {menuitems.map(x => {
                 return(
-                    <motion.li whileHover={{ scale: 1.5, translateX: 50 }} variants={variants_navItems} key={x.id}>
+                    <li key={x.id}>
                         <a href={x.url}>{x.name}</a>
-                    </motion.li>
+                    </li>
                 )
             })}
-        </motion.ul>
+        </ul>
     )
 }
 
@@ -75,10 +41,10 @@ const MenuButton = ({
     clicked
 }) => {
     return (
-        <motion.button whileHover={{scale: 1.1}} whileTap={{scale: 1.2}} className="MenuButton" onClick={() => onClick(!clicked)}>
+        <button className="MenuButton" onClick={() => onClick(!clicked)}>
             <div className="bar1"></div>
             <div className="bar2"></div>
             <div className="bar3"></div>
-        </motion.button>
+        </button>
     )
 }
